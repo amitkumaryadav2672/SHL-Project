3a5d1b17e770d05d587697e22cd0bc03c36cf833
AMIT KUMAR YADAV <126837768+amitkumaryadav2672@users.noreply.github.com>
2026-01-14 00:51:26 +0530

    Update api.js

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api", // backend URL
+  baseURL: "https://shl-project-d9gt.onrender.com/api", // backend URL
 });
 
 export const fetchRecommendations = (query) =>
